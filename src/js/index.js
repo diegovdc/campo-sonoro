02b327a50ef20a7dcec3bc4c035f8019d5ad322f
Diego <diego.vid.eco@gmail.com>
2020-04-19 19:37:22 -0500

    Removes everything but the server

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-
-import {data} from './model'
-import {audioElement} from './view'
-
-const main = document.getElementById('main')
-main.innerHTML = data.map(element => audioElement(element)).join('')
-
---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const token = process.env.FREESOUND_API_TOKEN;
+var cors = require("cors");
+app.use(cors());
+
+app.get("/data", (req, res) => {
+  const { query, page } = req.query;
+  console.log("query: ", query, "page: ", page);
+  axios
+    .get(
+      `https://freesound.org/apiv2/search/text?token=${token}&query=field+recordings+${query.replace(
+        " ",
+        "+"
+      )}&page=${page}&fields=username,tags,previews,url`
+    )
+    .then((r) => r.data)
+    .then((data) => res.json(data));
+});
+
+app.listen(port, () =>
+  console.log(`"Campo Sonoro" API listening on port ${port}!`)
+);
